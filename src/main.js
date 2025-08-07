c231923751912fba04e49af1acc96a4fab1d03f0
amir <amirtaki623@gmail.com>
2025-08-07 23:08:13 +0330

    styles p11

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ const cards = document.querySelectorAll(".card");
 cards.forEach((card) => {
     card.addEventListener("mousemove", (e)=> {
         const x = e.pageX - card.offsetLeft;
+      
         const y = e.pageY - card.offsetTop;
 
         card.style.setProperty("--x", x + 'px');
