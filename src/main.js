f22c7231c66515d026e57bbbf8e35299c33ec441
amir <amirtaki623@gmail.com>
2025-08-07 22:49:32 +0330

    js p4

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ cards.forEach((card) => {
     card.addEventListener("mousemove", (e)=> {
         const x = e.pageX - card.offsetLeft;
         const y = e.pageY - card.offsetTop;
+
+        card.style.setPropery("--x", x + 'px');
+        card.style.setPropery("--y", y + 'px');
     })
     
 })
